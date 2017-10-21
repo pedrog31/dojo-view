29e7b174138932c2d8eae4d549fc7173e17029af
Pedro Gallego <pedro.gallego@udea.edu.co>
2017-10-21 12:33:57 -0500

    all done

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
     <h2>home</h2>
+      <input v-model= "message" placeholder="edit" name = "entrada">
+      <button v-on:click="myFunction">Boton</button>
+      <h1> {{texto}} </h1>
   </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      message: "",
+      texto: ""
+  }
+},
+  methods: {
+    myFunction () {
+      this.texto = this.message
+    }
+}
+}
+
 </script>
 
 <style>
